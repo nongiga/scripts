326d959b550b88348091eae727e70d2c912d79d3
nongiga <nongiga@gmail.com>
2020-07-29 13:00:45 +0300

    played with analyzeRepeatingGenes, added geneDescription to summarize_alignment_reports

--- matlab/processing/summarize_alignment_reports.m
+++ matlab/processing/summarize_alignment_reports.m
@@ -1,8 +1,7 @@
 load('gap_data.mat');
 dl=filesep;
-Case=struct('Num',{},'SeqPlate',{},'GeneName',{},'Date',{},'NCov',{}, 'pval', {}, 'Reads',{});
+Case=struct('Num',{},'SeqPlate',{},'GeneName',{},'GeneDescription', {},'Date',{},'NCov',{}, 'pval', {}, 'Reads',{});
 
-AllCase=[]
 for ins=1:h
     
     id=[ num2str(pipevar.bp(ins)) moptions{pipevar.report_multi(ins)+1} ];
@@ -18,7 +17,6 @@ for ins=1:h
         FileName=[Path.Alignment dl TreeName dl 'alignments' id '.mat'];
         copyfile(FileName, [Path.Reports dl TreeName '_' id '.mat'])
         load(FileName, 'myCase')
-        AllCase(i)=myCase;
         for fn = fieldnames(Case)'
            Case(i).(fn{1}) = myCase.(fn{1});
         end
